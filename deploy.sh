0fd8a2b6fc9d3d2f329a02697262a455e279da44
rk365040 <rknaidu08@gmail.com>
2021-07-06 08:38:23 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "we are doing deployment"
-wget --user=ram --password=ram http://52.91.125.195:8081/nexus/service/local/repositories/ram/content/java/sparkjava-hello-world/1.0.3/sparkjava-hello-world-1.0.3.war
+wget --user=ram --password=$1 http://52.91.125.195:8081/nexus/service/local/repositories/ram/content/java/sparkjava-hello-world/1.0.3/sparkjava-hello-world-1.0.3.war
